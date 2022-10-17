49a30874c28addb2544cb17459efc8a06d452996
usernameYI <115969312+usernameYI@users.noreply.github.com>
2022-10-17 13:10:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: '#年年有俞',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '#年年有俞',
 
   PROVINCE: '河北',
   CITY: '定州',
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: wxba7aaa0494b47c80',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '9VLJVOc_uPHi6IcJAFD0MYF8f8zzKyZLtgPBHrjD7R8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-09',
       festivals: [
@@ -50,13 +50,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '9VLJVOc_uPHi6IcJAFD0MYF8f8zzKyZLtgPBHrjD7R8',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oB29s5t9R5mHj2SMhYaOhxfQ7www',
     }
   ],
 
